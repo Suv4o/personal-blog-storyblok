6f848d00b1c749430ff64b35579a1c7d784d602f
Aleks <aleksandar.trpkovski@gmail.com>
2023-03-21 22:32:28 +1100

    Set print width to be 120 in prettier

---
+++ storyblok/ComponentPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  blok: {
+    body: any
+    _uid: string
+  }
+}>()
+</script>
+
+<template>
+  <div>
+    <StoryblokComponent v-for="_blok in props.blok.body" :key="_blok._uid" :blok="_blok" />
+  </div>
+</template>
